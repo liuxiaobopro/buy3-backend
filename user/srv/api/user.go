224e68b81d80b27ea13c4bb032be3a462639c11e
liuxiaobo <isliuxiaobo@gmail.com>
2023-04-07 01:59:57 +0800

    ✨ feat: 测试user grpc

---
+++ user/srv/api/user.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"context"
+	"fmt"
+
+	"buy3/user/srv/proto"
+)
+
+type UserHandle struct {
+	*proto.UnimplementedUserServer
+}
+
+var _ proto.UserServer = (*UserHandle)(nil)
+
+// CreateUser implements proto.UserServer
+func (*UserHandle) CreateUser(c context.Context, in *proto.CreateUserRequest) (*proto.CreateUserResponse, error) {
+	fmt.Printf("in: %v", in)
+	return &proto.CreateUserResponse{}, nil
+}
+
+// GetUser implements proto.UserServer
+func (*UserHandle) GetUser(context.Context, *proto.GetUserRequest) (*proto.GetUserResponse, error) {
+	panic("unimplemented")
+}
+
+// UpdateUser implements proto.UserServer
+func (*UserHandle) UpdateUser(context.Context, *proto.UpdateUserRequest) (*proto.UpdateUserResponse, error) {
+	panic("unimplemented")
+}
